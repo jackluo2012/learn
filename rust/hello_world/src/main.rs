a07ea28048b8124363e57cf2f0a081719ecfce39
jackluo <net.webjoy@gmailcom>
2024-12-16 19:08:48 +0800

    rust for loop

--- rust/hello_world/src/main.rs
+++ rust/hello_world/src/main.rs
@@ -1,3 +1,75 @@
 fn main() {
-    println!("Hello, world!");
+    
+
+    //循环
+    // loop_test()
+    // while_test()
+    // for_test()
+    // for_test2()
+    for_test3()
+}
+
+// for range rev
+fn for_test3(){
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
+
+// for in 
+fn for_test2(){
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+}
+
+// for 测试
+fn for_test(){
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
+//while 测试
+fn while_test(){
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+//** loop 测试
+fn loop_test(){
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
