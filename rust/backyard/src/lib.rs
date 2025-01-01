de130c847fec9aad3a3454e8a676ab271ac17cf6
jackluo <net.webjoy@gmailcom>
2025-01-01 09:50:20 +0800

    rust trait deref drop

--- rust/backyard/src/lib.rs
+++ rust/backyard/src/lib.rs
@@ -37,3 +37,66 @@ pub mod utils {
 
     
 }
+
+pub trait Messager {
+    fn send(&self, msg: &str);
+}
+pub struct LimitTracker<'a, T: Messager> {
+    messenger: &'a T,
+    value: u32,
+    max: u32,
+}
+impl <'a, T> LimitTracker<'a, T>
+where
+    T: Messager,
+{
+    pub fn new(messenger: &T, max: u32) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+    pub fn set_value(&mut self, value: u32) {
+        self.value = value;
+        let percentage_of_max = self.value as f64 / self.max as f64;
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger.send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger.send("You are under your quota!");
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages:RefCell::new(vec![]),
+            }
+        }
+    }
+    impl Messager for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+        limit_tracker.set_value(80);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
