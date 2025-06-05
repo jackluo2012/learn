149075adb69e19505b4f898e0b4f3fb398ed7583
jackluo <net.webjoy@gmailcom>
2025-06-05 18:01:33 +0800

    graceful shutdown

--- rust/project/hello/src/lib.rs
+++ rust/project/hello/src/lib.rs
@@ -1,69 +1,104 @@
+// 导入需要的标准库组件
+// - mpsc: 多生产者单消费者通道，用于线程间通信
+// - Arc: 原子引用计数，用于在线程间共享数据
+// - Mutex: 互斥锁，确保数据的互斥访问
+// - thread: 线程管理模块
 use std::{sync::{mpsc,Arc,Mutex},thread};
 
 // Job 类型定义：可以在线程间安全传递和执行的任务
-// - Box<dyn FnOnce()> 表示一个堆分配的闭包
-// - Send 表示可以在线程间安全传递
-// - 'static 表示闭包中的所有引用具有静态生命周期
+// - Box<dyn FnOnce()>: 将闭包装箱到堆上
+// - Send: 保证类型可以安全地在线程间传递
+// - 'static: 确保闭包中的引用具有静态生命周期
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
-// 线程池结构体
-pub struct ThreadPool{
-    workers: Vec<Worker>,    // 工作线程集合
-    seeder: mpsc::Sender<Job>,  // 任务发送器
+// 线程池结构体：管理一组工作线程
+pub struct ThreadPool {
+    workers: Vec<Worker>,    // 存储所有工作线程
+    seeder: Option<mpsc::Sender<Job>>,  // 任务发送器，Option包装允许在析构时安全关闭
 }
 
 impl ThreadPool {
-    // 创建指定数量的线程池
+    // 创建新的线程池
+    // n: 指定要创建的工作线程数量
     pub fn new(n: usize) -> ThreadPool {
-        assert!(n > 0);  // 确保线程数大于0
+        assert!(n > 0);  // 线程数必须大于0
         
-        // 预分配工作线程vector的容量
+        // 预分配存储工作线程的向量
         let mut workers = Vec::with_capacity(n);
         
-        // 创建通道，用于发送任务
+        // 创建消息通道，用于向工作线程发送任务
         let (seeder, receiver) = mpsc::channel();
         
-        // 将接收器包装在 Arc 和 Mutex 中
-        // - Arc: 允许多个工作线程共享所有权
-        // - Mutex: 确保同一时间只有一个线程可以接收任务
+        // 包装接收端
+        // - Arc使得多个工作线程可以共享接收端
+        // - Mutex确保在同一时刻只有一个线程能接收任务
         let receiver = Arc::new(Mutex::new(receiver));
         
-        // 创建n个工作线程
+        // 创建指定数量的工作线程
         for id in 0..n { 
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
         
-        ThreadPool { workers, seeder }
+        ThreadPool { workers, seeder: Some(seeder) }
     }
 
-    // 执行任务的方法
-    // F: 必须是可以在线程间安全传递且没有引用的闭包
+    // 向线程池提交任务
+    // F: 符合 FnOnce + Send + 'static 约束的闭包类型
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        let job = Box::new(f);  // 将任务装箱
-        self.seeder.send(job).unwrap();  // 发送任务到通道
+        let job = Box::new(f);  // 将任务闭包装箱
+        self.seeder.as_ref().unwrap().send(job).unwrap();  // 将任务发送给工作线程
     }
 }
 
-// 工作线程结构体
+// 实现 Drop trait，用于线程池的清理工作
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        // 首先丢弃发送者，这会导致接收端收到错误，从而使工作线程退出
+        drop(self.seeder.take());
+        
+        // 等待所有工作线程完成
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            // 取出线程句柄并等待其结束
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            } 
+        }
+    }       
+}
+
+// 工作线程结构体：表示线程池中的一个工作线程
 struct Worker {
     id: usize,  // 工作线程的唯一标识符
-    thread: thread::JoinHandle<()>,  // 线程句柄
+    thread: Option<thread::JoinHandle<()>>,  // 线程句柄，Option允许在清理时取出所有权
 }
 
 impl Worker {
     // 创建新的工作线程
+    // id: 工作线程标识符
+    // receiver: 共享的任务接收器
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        // 创建实际的工作线程
+        // 创建新线程并开始任务循环
         let thread = thread::spawn(move || loop {
-            // 获取互斥锁并接收任务
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {id} got a job; executing.");
-            job();  // 执行任务
+            // 尝试接收任务
+            let message = receiver.lock().unwrap().recv();
+            
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();  // 执行收到的任务
+                }
+                Err(_) => {
+                    // 如果接收错误（通道已关闭），终止线程
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
         });
         
-        Worker { id, thread }
+        Worker { id, thread: Some(thread) }
     }
 }
--- rust/project/hello/src/main.rs
+++ rust/project/hello/src/main.rs
@@ -1,18 +1,23 @@
+// 导入必要的模块
 use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-    thread,
-    time::{Duration},
+    fs, // 文件系统操作
+    io::{prelude::*, BufReader}, // 输入/输出相关
+    net::{TcpListener, TcpStream}, // 网络编程相关
+    thread, // 多线程相关
+    time::{Duration}, // 时间相关
 };
+// 导入自定义的线程池模块
 use hello::ThreadPool;
 
+// 主函数
 fn main() {
+    // 绑定监听地址和端口
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     
+    // 创建一个包含 4 个线程的线程池
     let pool = ThreadPool::new(4);
     
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
         // 加入多线程
         pool.execute(||  {
@@ -20,29 +25,45 @@ fn main() {
         });
         
     }
+    // 程序关闭提示
+    println!("Shutting down.");
 }
 
-fn handle_connection(mut stream: TcpStream) { 
-
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader
-        .lines().next().unwrap().unwrap();
+// 处理连接函数
+fn handle_connection(mut stream: TcpStream) {
+    // 创建一个缓冲区来存储客户端发送的数据
+    let mut buffer = [0; 1024];
+    // 从流中读取数据到缓冲区
+    stream.read(&mut buffer).unwrap();
+    // 定义 GET 请求的字节数组
+    let get = b"GET / HTTP/1.1\r\n";
+    // 定义 sleep 请求的字节数组
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     
    
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    // 根据请求类型选择不同的响应
+    let (status_line, filename) = if buffer.starts_with(get) {
+        // 如果是 GET 请求，返回 200 OK 和 hello.html
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        // 如果是 sleep 请求，休眠 5 秒，然后返回 200 OK 和 hello.html
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        // 否则返回 404 NOT FOUND 和 404.html
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
+    // 读取指定文件内容
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
+    // 构建 HTTP 响应
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n\n{contents}");
 
+    // 将响应写入流中
     stream.write_all(response.as_bytes()).unwrap();
+    // 刷新流，确保数据发送出去
+    stream.flush().unwrap();
 
 }
